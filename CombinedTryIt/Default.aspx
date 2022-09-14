1a71d35396915ebebbb7756bb181db40f4f5132e
MarDiaz64 <90005561+MarDiaz64@users.noreply.github.com>
2022-09-13 20:14:56 -0700

    Combined TryIt page

---
+++ CombinedTryIt/Default.aspx
@@ -0,0 +1,195 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="CombinedTryIt.Default" %>
+<%@ OutputCache Duration="20" VaryByParam="*" %>
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Assignment5</title>
+    <!--<link href="CSS/StyleSheet.css" rel="stylesheet" type="text/css" />-->
+    <style type ="text/css">
+        #Input {
+            background-color: rgb(144,198,226);
+            border-radius:10px;
+            border: 1px solid #000;
+            width:250px;
+            height:100%;
+            
+        }
+
+        body {
+            background-color:rgb(144,198,226);
+        }
+        header {
+            background-color: rgb(0,89,134);
+            padding: 30px;
+            text-align: left;
+            font-size: 35px;
+            color: white;
+        }
+        #ComName {
+            padding-right: 50px;
+            font-size:20px;
+        }
+        {
+            box-sizing: border-box;
+        }
+        .column {
+            float: left;
+            width: 75%;
+        }
+        .column2 {
+            float: left;
+            width: 25%;
+        }
+        .column3 {
+            float: left;
+            width: 16%;
+            margin:1px;
+        }
+
+        .row:after {
+            content: "";
+            display: table;
+            clear: both;
+        }
+        .alert {
+          padding: 20px;
+          background-color: #f44336;
+          color: white;
+          display:none;
+        }
+        #middle {
+            height:500px;
+        }
+
+        .closebtn {
+          margin-left: 15px;
+          color: white;
+          font-weight: bold;
+          float: right;
+          font-size: 22px;
+          line-height: 20px;
+          cursor: pointer;
+          transition: 0.3s;
+        }
+        footer {
+            background-color: rgb(0,89,134);
+            padding: 30px;
+            text-align: left;
+            font-size: 15px;
+            color: white;
+        
+        }
+
+        .closebtn:hover {
+          color: black;
+        }
+        .box {
+            background-color: rgb(0,89,134);
+            padding: 10px;
+            text-align: center;
+            font-size: 15px;
+            color: white;
+            border-width:1px;
+            border-radius:8px;
+            margin:5px;
+        }
+        .day {
+            background-color:rgb(144,198,226);
+            padding: 10px;
+            text-align: center;
+            font-size: 15px;
+            color: white;
+            border-width:1px;
+            border-radius:8px;
+            margin:5px;
+        }
+        #Day1 {
+            white-space: pre-wrap;
+        }
+        #Day2 {
+            white-space: pre-wrap;
+        }
+        #Day3 {
+            white-space: pre-wrap;
+        }
+        #Day4 {
+            white-space: pre-wrap;
+        }
+        #Day5 {
+            white-space: pre-wrap;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <header> 
+            <asp:Image ID="Image1" runat="server" Height="61px" ImageUrl="Compass.png" Width="62px" />
+            <label id="ComName">Compass Weather Co.</label>
+            <center><asp:TextBox ID="Input" runat="server"></asp:TextBox>
+            <asp:ImageButton ID="ImageButton1" runat="server"  ImageUrl="search.png" OnClick="ImageButton1_Click" Height="30px" Width="30px" />
+            </center></header>
+            
+        <div id="Err" class="alert" runat="server"><span class="closebtn" onclick="this.parentElement.style.display='none';">&times;</span> Invalid Zipcode!</div>
+        <div class="row">
+        <div class="column">
+            <div class ="box">
+            <h2>Five Day Forecast</h2>
+            <asp:Label ID="Location" runat="server" Text=""></asp:Label>
+            <div class="row">
+                <div class="column3 day">
+                    <center>
+                    <asp:Image ID="ImgDay1" runat="server" />
+                        <br />
+                    <asp:Label ID="Day1" runat="server" Text=""></asp:Label>
+                    </center>  
+                </div>
+                <div class="column3 day">
+                    <center>
+                    <asp:Image ID="ImgDay2" runat="server" />
+                        <br />
+                    <asp:Label ID="Day2" runat="server" Text=""></asp:Label>
+                    </center>  
+                </div>
+                <div class="column3 day">
+                    <center>
+                    <asp:Image ID="ImgDay3" runat="server" />
+                        <br />
+                    <asp:Label ID="Day3" runat="server" Text=""></asp:Label>
+                    </center>  
+                </div>
+                <div class="column3 day" >
+                    <center>
+                    <asp:Image ID="ImgDay4" runat="server" />
+                        <br />
+                    <asp:Label ID="Day4" runat="server" Text=""></asp:Label>
+                    </center>  
+                </div>
+                <div class="column3 day">
+                    <center>
+                    <asp:Image ID="ImgDay5" runat="server" />
+                        <br />
+                    <asp:Label ID="Day5" runat="server" Text=""></asp:Label>
+                    </center>  
+                </div>
+            </div>
+            </div>
+        </div>
+        <div class="column2">
+            <div class="box">
+                <h2>Annual Solar:</h2>
+                <p>Ghi: <asp:Label ID="ghi" runat="server" Text=""></asp:Label></p>
+                <p>Dni: <asp:Label ID="dni" runat="server" Text=""></asp:Label></p>
+                <p>Lat_Tilt: <asp:Label ID="tilt" runat="server" Text=""></asp:Label></p>
+            </div>
+            <div class="box">
+                <h2>Today's Pollution:</h2>
+                <p>Air Quality: <asp:Label ID="AQ" runat="server" Text=""></asp:Label></p>
+            </div>
+        </div>
+    </div>
+        <footer><p>By: Maryannette Diaz, 2022</p><p>Credit For Icons: <asp:HyperLink ID="HyperLink1" runat="server">https://icons8.com</asp:HyperLink>
+            </p></footer>
+    </form>
+</body>
+</html>
---
+++ CombinedTryIt/Default.aspx.cs
@@ -0,0 +1,258 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CombinedTryIt
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        HttpCookie cookie;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Input.Text =="") {
+                HttpCookie cookie = Request.Cookies["myCookie"];
+                if (cookie != null)
+                {
+                    Input.Text = cookie["input"];
+                    if (cookie["ERROR"] != "e")
+                    {
+                        Day1.Text = cookie["DAY1"].Replace("%0a","\n");
+                        Day2.Text = cookie["DAY2"].Replace("%0a", "\n");
+                        Day3.Text = cookie["DAY3"].Replace("%0a", "\n");
+                        Day4.Text = cookie["DAY4"].Replace("%0a", "\n");
+                        Day5.Text = cookie["DAY5"].Replace("%0a", "\n");
+                        ImgDay1.ImageUrl = cookie["IMGDAY1"];
+                        ImgDay2.ImageUrl = cookie["IMGDAY2"];
+                        ImgDay3.ImageUrl = cookie["IMGDAY3"];
+                        ImgDay4.ImageUrl = cookie["IMGDAY4"];
+                        ImgDay5.ImageUrl = cookie["IMGDAY5"];
+                        ghi.Text = cookie["GHI"];
+                        dni.Text = cookie["DNI"];
+                        tilt.Text = cookie["TILT"];
+                        AQ.Text = cookie["AQ"];
+                        Location.Text = cookie["Location"];
+
+                    }
+                }
+
+            }
+
+        }
+        /*Daily Maximum Temperaturein F: 62
+Daily Minimum Temperaturein F: 42
+Probability of Precipitation in %: 3
+Weather Type, Coverage, and Intensity: Weather - summary: Mostly Cloudy
+Conditions Icons: http://forecast.weather.gov/images/wtf/bkn.jpg
+Watches, Warnings, and Advisories: */
+        protected Forecast[] parseForecast(string[] forcast) {
+            Forecast[] data = new Forecast[5];
+            data[0] = new Forecast();
+            data[1] = new Forecast();
+            data[2] = new Forecast();
+            data[3] = new Forecast();
+            data[4] = new Forecast();
+            for (int i=0; i<5; i++) {
+                string[] split = forcast[i].Split(new string[] { "\n" }, StringSplitOptions.None);
+                data[i].tempMax = Int32.Parse(Regex.Matches(split[0], @"[+-]?\d+(\.\d+)?")[0].Value);
+                data[i].tempMin = Int32.Parse(Regex.Matches(split[1], @"[+-]?\d+(\.\d+)?")[0].Value);
+                data[i].precip = Int32.Parse(Regex.Matches(split[2], @"[+-]?\d+(\.\d+)?")[0].Value);
+                string temp = split[3].Substring(split[3].IndexOf(":")+1);
+                data[i].summary = temp.Substring(temp.IndexOf(":")+1).Trim();
+                data[i].image = split[4].Substring(split[4].IndexOf(":")+1).Trim();
+                data[i].hazards = split[5].Substring(split[5].IndexOf(":")+1).Trim();
+            }
+            return data;
+        }
+        protected void fiveDayForecast(string zip)
+        {
+            WeatherService.Service1Client client = new WeatherService.Service1Client();
+            string[] output = client.Weather5day(zip);
+            if (output == null)
+            {
+                cookie["ERROR"] = "e";
+                Input.BorderColor = System.Drawing.Color.Red;
+                Input.BorderWidth = 2;
+                Err.Attributes.CssStyle["display"] = "block";
+            }
+            else
+            {
+                Forecast[] temp = parseForecast(output);
+                //Forecasts:
+                Day1.Text = temp[0].summary + "\n" + temp[0].tempMin + "/" + temp[0].tempMax + "F\nPrecipitation:" + temp[0].precip + "%";
+                Day2.Text = temp[1].summary + "\n" + temp[1].tempMin + "/" + temp[1].tempMax + "F\nPrecipitation:" + temp[1].precip + "%";
+                Day3.Text = temp[2].summary + "\n" + temp[2].tempMin + "/" + temp[2].tempMax + "F\nPrecipitation:" + temp[2].precip + "%";
+                Day4.Text = temp[3].summary + "\n" + temp[3].tempMin + "/" + temp[3].tempMax + "F\nPrecipitation:" + temp[3].precip + "%";
+                Day5.Text = temp[4].summary + "\n" + temp[4].tempMin + "/" + temp[4].tempMax + "F\nPrecipitation:" + temp[4].precip + "%";
+
+                //Images For Forecasts:
+                ImgDay1.ImageUrl = temp[0].image;
+                ImgDay2.ImageUrl = temp[1].image;
+                ImgDay3.ImageUrl = temp[2].image;
+                ImgDay4.ImageUrl = temp[3].image;
+                ImgDay5.ImageUrl = temp[4].image;
+
+                //cookie stuff:
+                cookie["DAY1"] = Day1.Text;
+                cookie["DAY2"] = Day2.Text;
+                cookie["DAY3"] = Day3.Text;
+                cookie["DAY4"] = Day4.Text;
+                cookie["DAY5"] = Day5.Text;
+                cookie["IMGDAY1"] = ImgDay1.ImageUrl;
+                cookie["IMGDAY2"] = ImgDay2.ImageUrl;
+                cookie["IMGDAY3"] = ImgDay3.ImageUrl;
+                cookie["IMGDAY4"] = ImgDay4.ImageUrl;
+                cookie["IMGDAY5"] = ImgDay5.ImageUrl;
+            }
+        }
+        protected void currentPollution(string zip) {
+            string url = @"http://localhost:57214/Service1.svc/current?zipcode=" + zip;
+            HttpWebRequest request2 = (HttpWebRequest)WebRequest.Create(url);
+            WebResponse response2 = request2.GetResponse();
+            Stream dataStream2 = response2.GetResponseStream();
+            StreamReader sreader2 = new StreamReader(dataStream2);
+            var responsereader2 = sreader2.ReadToEnd();
+            response2.Close();
+            string temp = responsereader2;
+            if (temp.Equals(""))
+            {
+                cookie["ERROR"] = "e";
+                Input.BorderColor = System.Drawing.Color.Red;
+                Input.BorderWidth = 2;
+                Err.Attributes.CssStyle["display"] = "block";
+            }
+            else
+            {
+                PolOutput temp1 = JsonConvert.DeserializeObject<PolOutput>(responsereader2);
+                //add more display info her
+                AQ.Text = temp1.days[0].values[0].indexStr;
+                Location.Text = temp1.lo.name + ", " + temp1.lo.zipcode + ",U.S.";
+                cookie["AQ"] = AQ.Text;
+                cookie["Location"] = Location.Text;
+            }
+        }
+        protected void solarInformation(string zip) {
+            string url = @"http://localhost:50089/Service1.svc/Solar?zip=" + zip;
+            HttpWebRequest request2 = (HttpWebRequest)WebRequest.Create(url);
+            WebResponse response2 = request2.GetResponse();
+            Stream dataStream2 = response2.GetResponseStream();
+            StreamReader sreader2 = new StreamReader(dataStream2);
+            string responsereader2 = sreader2.ReadToEnd();
+            response2.Close();
+            string temp = responsereader2;
+            if (temp.Equals(""))
+            {
+                cookie["ERROR"] = "e";
+                Input.BorderColor = System.Drawing.Color.Red;
+                Input.BorderWidth = 2;
+                Err.Attributes.CssStyle["display"] = "block";
+            }
+            else
+            {
+                SunOutput temp1 = JsonConvert.DeserializeObject<SunOutput>(responsereader2);
+                ghi.Text = temp1.avg_ghi.annual.ToString();
+                dni.Text = temp1.avg_dni.annual.ToString();
+                tilt.Text = temp1.avg_lat_tilt.annual.ToString();
+                cookie["GHI"] = ghi.Text;
+                cookie["DNI"] = dni.Text;
+                cookie["TILT"] = tilt.Text;
+            }
+        }
+
+        protected void ImageButton1_Click(object sender, ImageClickEventArgs e)
+        {
+            //create cookie:
+            cookie = new HttpCookie("myCookie");
+            cookie["input"] = Input.Text;
+            cookie["ERROR"] = "a";
+            cookie.Expires = DateTime.Now.AddMinutes(1);
+            //rest of the actions
+            Input.BorderColor = System.Drawing.Color.Black;
+            Input.BorderWidth = 1;
+            Err.Attributes.CssStyle["display"] = "none";
+            fiveDayForecast(Input.Text);
+            solarInformation(Input.Text);
+            currentPollution(Input.Text);
+
+            //finish cookie
+            Response.Cookies.Add(cookie);
+        }
+    }
+    //collect forecast information:
+    public class Forecast {
+        public int tempMax { get; set; }
+        public int tempMin { get; set; }
+        public int precip { get; set; }
+        public string summary { get; set; }
+        public string image { get; set; }
+        public string hazards { get; set; }
+    }
+    // Deserialize Solar information:
+    public class SunOutput
+    {
+        public int index { get; set; }
+        public Avgs avg_dni { get; set; }
+        public Avgs avg_ghi { get; set; }
+        public Avgs avg_lat_tilt { get; set; }
+    }
+    public class Avgs
+    {
+        public double annual { get; set; }
+        public Monthly monthly { get; set; }
+        public Seasonal seasonally { get; set; }
+    }
+    public class Seasonal
+    {
+        public double winter { get; set; }
+        public double spring { get; set; }
+        public double summer { get; set; }
+        public double fall { get; set; }
+    }
+    //deserialize pollution information
+    public class PolOutput
+    {
+        public location lo;
+        public Day[] days;
+        public string PollutanUnits { get; set; }
+    }
+    public class Day
+    {
+        public int dt { get; set; }
+        public measurement[] values;
+    }
+    public class location
+    {
+        public string zipcode { get; set; }
+        public string name { get; set; }
+        public double lat { get; set; }
+        public double lon { get; set; }
+    }
+    public class measurement
+    {
+        public string name;
+        public double value;
+        public int index; 
+        public string indexStr;
+    }
+    public class Monthly
+    {
+        public double jan { get; set; }
+        public double feb { get; set; }
+        public double mar { get; set; }
+        public double apr { get; set; }
+        public double may { get; set; }
+        public double jun { get; set; }
+        public double jul { get; set; }
+        public double aug { get; set; }
+        public double sep { get; set; }
+        public double oct { get; set; }
+        public double nov { get; set; }
+        public double dec { get; set; }
+    }
+}
