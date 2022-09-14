2af5d984315e952b32e5034e3d346b213cf32c16
MarDiaz64 <90005561+MarDiaz64@users.noreply.github.com>
2022-09-13 20:16:24 -0700

    SolarAPI
    wrapper API

---
+++ SolarAPI/IService1.cs
@@ -0,0 +1,36 @@
+﻿using System.ServiceModel;
+using System.ServiceModel.Web;
+
+namespace SolarAPI
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    {
+
+        [OperationContract]
+        [WebGet(UriTemplate = "Solar?zip={zipcode}", ResponseFormat = WebMessageFormat.Json)]
+        Output SolarInfo(string zipcode);
+    }
+
+    public class Output
+    {
+        public int index { get; set; }
+        public Avgs avg_dni { get; set; }
+        public Avgs avg_ghi { get; set; }
+        public Avgs avg_lat_tilt { get; set; }
+    }
+    public class Avgs
+    {
+        public double annual { get; set; }
+        public Monthly monthly { get; set; }
+        public Seasonal seasonally { get; set; }
+    }
+    public class Seasonal
+    {
+        public double winter { get; set; }
+        public double spring { get; set; }
+        public double summer { get; set; }
+        public double fall { get; set; }
+    }
+}
